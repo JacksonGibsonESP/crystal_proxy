a52c15db59374e70d27fa1c96e9aaba95ca63094
JacksonGibsonESP <JacksonGibsonESP@gmail.com>
2018-01-08 21:07:02 +0300

    Bool queries added.

--- src/main/java/ru/mai/dep810/webapp/repository/ElasticRepository.java
+++ src/main/java/ru/mai/dep810/webapp/repository/ElasticRepository.java
@@ -1,17 +1,22 @@
 package ru.mai.dep810.webapp.repository;
 
+import org.apache.commons.lang.StringUtils;
 import org.elasticsearch.action.ActionFuture;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.common.text.Text;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ru.mai.dep810.webapp.model.SearchResult;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
@@ -24,22 +29,69 @@ public class ElasticRepository {
     @Value("${elastic.index.name}")
     String indexName;
 
-    public SearchResult<String> findPosts(String query) {
+    public SearchResult<Map<String, Object>> findPosts(
+            String query,
+            String chemicalElement,
+            String chemicalFormula,
+            String crystalSystem,
+            String radiusType,
+            String spaceGroup,
+            int from,
+            int size) {
+        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+
+        if (StringUtils.isNotBlank(query)) {
+            queryBuilder.must(QueryBuilders.queryStringQuery(query));
+        }
+        if (StringUtils.isNotBlank(chemicalElement)) {
+            queryBuilder.must(QueryBuilders.queryStringQuery(chemicalElement));
+        }
+        if (StringUtils.isNotBlank(chemicalFormula)) {
+            queryBuilder.must(QueryBuilders.queryStringQuery(chemicalFormula));
+        }
+        if (StringUtils.isNotBlank(crystalSystem)) {
+            queryBuilder.must(QueryBuilders.queryStringQuery(crystalSystem));
+        }
+        if (StringUtils.isNotBlank(radiusType)) {
+            queryBuilder.must(QueryBuilders.queryStringQuery(radiusType));
+        }
+        if (StringUtils.isNotBlank(spaceGroup)) {
+            queryBuilder.must(QueryBuilders.queryStringQuery(spaceGroup));
+        }
+
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("attachment.content").highlighterType("plain");
 
         ActionFuture<SearchResponse> execute = client.prepareSearch(indexName)
-                .setQuery(QueryBuilders.queryStringQuery(query))
-                .setFetchSource(new String[] {"_id"}, new String[] {})
+                .setQuery(queryBuilder)
+                .setFetchSource(new String[] {"_id", "attachment.content"}, new String[] {})
+                .setFrom(from)
+                .setSize(size)
+                .highlighter(highlightBuilder)
                 .execute();
 
         return convertToSearchResult(execute);
     }
 
-    private SearchResult<String> convertToSearchResult(ActionFuture<SearchResponse> execute) {
+    private SearchResult<Map<String, Object>> convertToSearchResult(ActionFuture<SearchResponse> execute) {
         try {
             SearchResponse searchResponse = execute.get();
-            List<String> rows = Arrays.stream(searchResponse.getHits().getHits())
-                    .map(SearchHit::getId)
+
+            List<Map<String, Object>> rows = Arrays.stream(searchResponse.getHits().getHits())
+                    .map(e -> {
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("_id", e.getId());
+                        if (!e.getHighlightFields().isEmpty()) {
+                            map.put("highlight",
+                                    Arrays.stream(e.getHighlightFields().get("attachment.content").getFragments())
+                                            .map(Text::string).collect(Collectors.toList())
+                            );
+                        }
+                        map.put("attachment.content", ((Map<String, Object>) e.getSourceAsMap().get("attachment")).get("content"));
+                        return map;
+                    })
                     .collect(Collectors.toList());
+
             return new SearchResult<>(rows, searchResponse.getHits().getTotalHits());
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
--- src/main/java/ru/mai/dep810/webapp/rest/PostRestController.java
+++ src/main/java/ru/mai/dep810/webapp/rest/PostRestController.java
@@ -17,8 +17,17 @@ public class PostRestController {
     private ElasticRepository elasticRepository;
 
     @RequestMapping(value = "/api/search", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
-    public SearchResult<?> searchPosts(@RequestParam("q") String query) {
-        return elasticRepository.findPosts(query);
+    public SearchResult<?> searchPosts(
+            @RequestParam(value = "Query", required = false) String query,
+            @RequestParam(value = "ChemicalElement", required = false) String chemicalElement,
+            @RequestParam(value = "ChemicalFormula", required = false) String chemicalFormula,
+            @RequestParam(value = "CrystalSystem", required = false) String crystalSystem,
+            @RequestParam(value = "RadiusType", required = false) String radiusType,
+            @RequestParam(value = "SpaceGroup", required = false) String spaceGroup,
+            @RequestParam(value = "from", required = false, defaultValue = "0") int from,
+            @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
+        return elasticRepository.findPosts(
+                query, chemicalElement, chemicalFormula, crystalSystem, radiusType, spaceGroup, from, size);
     }
 
 }
